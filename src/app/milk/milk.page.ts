a146637624b9e02fe9f30f784ff64f9cbeb64bec
autosapien <autosapien>
2018-09-06 15:52:35 +0530

    Add month and year selectors

--- src/app/milk/milk.page.ts
+++ src/app/milk/milk.page.ts
@@ -9,32 +9,26 @@ import { DeptDataService } from '../services/dept-data.service';
 })
 export class MilkPage {
 
-    selMonth: number = new Date().getMonth();
-    selYear: number = new Date().getFullYear();
-
-    yearList: Array<{ id: number, year: string }> = [
-        { id: 2018, year: '2018' },
-        { id: 2019, year: '2019' },
-        { id: 2020, year: '2020' }
+    selMonth = String(new Date().getMonth());
+    selYear = String(new Date().getFullYear());
+    yearList = [ '2018', '2019', '2020' ];
+    monthList: Array<{ id: string, month: string }> = [
+        { id: '0', month: 'January' },
+        { id: '1', month: 'February' },
+        { id: '2', month: 'March' },
+        { id: '3', month: 'April' },
+        { id: '4', month: 'May' },
+        { id: '5', month: 'June' },
+        { id: '6', month: 'July' },
+        { id: '7', month: 'August' },
+        { id: '8', month: 'September' },
+        { id: '9', month: 'October' },
+        { id: '10', month: 'November' },
+        { id: '11', month: 'December' },
     ];
 
-    monthList: Array<{ id: number, month: string }> = [
-        { id: 0, month: 'January' },
-        { id: 1, month: 'February' },
-        { id: 2, month: 'March' },
-        { id: 3, month: 'April' },
-        { id: 4, month: 'May' },
-        { id: 5, month: 'June' },
-        { id: 6, month: 'July' },
-        { id: 7, month: 'August' },
-        { id: 8, month: 'September' },
-        { id: 9, month: 'October' },
-        { id: 10, month: 'November' },
-        { id: 11, month: 'December' },
-    ];
-
-    monthAlertOpts: { title: string } = {title: 'Select Month'};
-    yearAlertOpts: { title: string } = {title: 'Select Year'};
+    monthAlertOpts = {title: 'Select Month'};
+    yearAlertOpts = {title: 'Select Year'};
 
     constructor(
         public nav: NavController,
