fdc82942fdbbc18ff146378bf76ebad2fda99062
autosapien <autosapien>
2018-09-07 08:07:18 +0530

    Cleanup

--- src/app/milk/milk.page.ts
+++ src/app/milk/milk.page.ts
@@ -1,17 +1,18 @@
 import { Component } from '@angular/core';
 import { NavController, Platform, LoadingController, AlertController, Events } from '@ionic/angular';
+import { ModalController } from '@ionic/angular';
 import { DeptDataService } from '../services/dept-data.service';
 
 @Component({
-  selector: 'app-about',
-  templateUrl: 'milk.page.html',
-  styleUrls: ['milk.page.scss']
+    selector: 'app-about',
+    templateUrl: 'milk.page.html',
+    styleUrls: ['milk.page.scss']
 })
 export class MilkPage {
 
     selMonth = String(new Date().getMonth());
     selYear = String(new Date().getFullYear());
-    yearList = [ '2018', '2019', '2020' ];
+    yearList = ['2018', '2019', '2020'];
     monthList: Array<{ id: string, month: string }> = [
         { id: '0', month: 'January' },
         { id: '1', month: 'February' },
@@ -27,21 +28,17 @@ export class MilkPage {
         { id: '11', month: 'December' },
     ];
 
-    monthAlertOpts = {title: 'Select Month'};
-    yearAlertOpts = {title: 'Select Year'};
+    monthAlertOpts = { title: 'Select Month' };
+    yearAlertOpts = { title: 'Select Year' };
 
     constructor(
-        public nav: NavController,
+        public navController: NavController,
         public platform: Platform,
         public loadingCtrl: LoadingController,
-        public events: Events,
         public alertController: AlertController,
+        public modalController: ModalController,
         public deptService: DeptDataService
-      ) {
-        // check if a dairy has been set for the user. Without a dairy send the user back to the home screen
-        events.subscribe('tabs:onViewMilkTab', () => {
-            console.log('onviewmilktab Event');
-        });
+    ) {
     }
 
 }
